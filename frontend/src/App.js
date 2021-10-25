70b36558ba279a68efc80a517d1965c39983bb15
Oliver Ramstedt <ora@capasystems.com>
2021-10-25 11:09:08 +0200

    Setup react app, and application uses array instead of static products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,51 @@
+import React from 'react';
+import data from './data';
+
 function App() {
   return (
     <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="index.html">Ora</a>
+                    <a className="brand" href="/">Ora</a>
                 </div>
 
                 <div>
-                    <a href="cart.html">Cart</a>
-                    <a href="login.html">Sign In</a>
+                    <a href="/cart">Cart</a>
+                    <a href="/login">Sign In</a>
                 </div>
             </header>
 
             <main>
-                <div className="row center">
-                    <div className="card">
-                        <a href="product.html">
-                            <img className="medium" src="./images/products/p1.jpg" alt="product"></img>
-                        </a>
-                        <div className="card-body">
-                            <a href="product.html">
-                                <h2>Bonsai Tree</h2>
-                            </a>
-                            <div className="rating">
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star-half-o"></i></span>
-                                <span><i className="fa fa-star-o"></i></span>
-
-                            </div>
-                            <div className="price">
-                                $120
-                            </div>
-                        </div>
-
+                <div>
+                    <div className="row center">
+                        {
+                            data.products.map((product) => (
+                                <div key={product._id} className="card">
+                                    <a href={`/product/${product._id}`}>
+                                        <img
+                                         className="medium" 
+                                         src={product.image} 
+                                         alt={product.name}>
+                                        </img>
+                                    </a>
+                                    <div className="card-body">
+                                        <a href={`/product/${product._id}`}>
+                                            <h2>{product.name}</h2>
+                                        </a>
+                                        <div className="rating">
+                                            <span><i className="fa fa-star"></i></span>
+                                            <span><i className="fa fa-star"></i></span>
+                                            <span><i className="fa fa-star"></i></span>
+                                            <span><i className="fa fa-star-half-o"></i></span>
+                                            <span><i className="fa fa-star-o"></i></span>
+                                        </div>
+                                        <div className="price">
+                                            {product.price}kr
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </main>
---
+++ frontend/src/data.js
@@ -0,0 +1,39 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Test Product',
+            category: 'Test Category',
+            image: './images/products/p1.jpg',
+            price: 120,
+            species: 'Test Species',
+            rating: 4.5,
+            numReviews: 10,
+            description: "This is a test product"
+        },
+        {
+            _id:'2',
+            name: 'Test Product2',
+            category: 'Test Category',
+            image: './images/products/p2.jpg',
+            price: 100,
+            species: 'Test Species',
+            rating: 4.7,
+            numReviews: 12,
+            description: "This is another test product"
+        },
+        {
+            _id:'3',
+            name: 'Test Product3',
+            category: 'Test Category',
+            image: './images/products/p3.jpg',
+            price: 200,
+            species: 'Test Species',
+            rating: 4.0,
+            numReviews: 23,
+            description: "This is another test product"
+        },
+    ],
+};
+
+export default data;
