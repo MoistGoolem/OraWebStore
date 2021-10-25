2dcb8b481d9afa2e7697f920fe603f78d1a3eb67
Oliver Ramstedt <ora@capasystems.com>
2021-10-25 10:39:22 +0200

    Created reacvt app and moved html and css to new folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="index.html">Ora</a>
+                </div>
+
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="login.html">Sign In</a>
+                </div>
+            </header>
+
+            <main>
+                <div className="row center">
+                    <div className="card">
+                        <a href="product.html">
+                            <img className="medium" src="./images/products/p1.jpg" alt="product"></img>
+                        </a>
+                        <div className="card-body">
+                            <a href="product.html">
+                                <h2>Bonsai Tree</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star-half-o"></i></span>
+                                <span><i className="fa fa-star-o"></i></span>
+
+                            </div>
+                            <div className="price">
+                                $120
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+
+            <footer className="row center">
+                All rights reserved
+            </footer>
+        </div>
   );
 }
-
 export default App;
