43d6bb7e8e6334e8e4e6a2c832a0c26450b377fd
Oliver Ramstedt <ora@capasystems.com>
2021-10-29 13:50:32 +0200

    Setup cart functionality with reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -10,12 +10,12 @@ function App() {
         <div className="grid-container">
             <header className="row">
                 <div>
-                    <a className="brand" href="/">Ora</a>
+                    <Link className="brand" to="/">Ora</Link>
                 </div>
 
                 <div>
-                    <a href="/cart">Cart</a>
-                    <a href="/login">Sign In</a>
+                    <Link to="/cart">Cart</Link>
+                    <Link to="/login">Sign In</Link>
                 </div>
             </header>
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
@@ -7,6 +8,7 @@ const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
