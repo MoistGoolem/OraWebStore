750fd63af94fffbc4a6d2d6d9532db3c890bb6e5
Oliver Ramstedt <ora@capasystems.com>
2021-10-29 15:01:19 +0200

    Added real cart screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -87,13 +87,11 @@ export default function ProductScreen(props) {
                                                         // @ts-ignore
                                                         onChange={(e) => setQty(e.target.value)}
                                                     >
-                                                        {[...Array(product.countInStock).keys()].map(
-                                                            (x) => (
-                                                                <option key={x + 1} value={x + 1}>
-                                                                    {x + 1}
-                                                                </option>
-                                                            )
-                                                        )}
+                                                        {[...Array(product.countInStock).keys()].map((x) => (
+                                                            <option key={x + 1} value={x + 1}>
+                                                                {x + 1}
+                                                            </option>
+                                                        ))}
                                                     </select>
                                                 </div>
 
