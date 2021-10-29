cf4187683d232ef526ae2c8f3ce2e0c83ee102c4
Oliver Ramstedt <ora@capasystems.com>
2021-10-29 14:02:28 +0200

    Added badge for number of cart items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <div className="grid-container">
-            <header className="row">
-                <div>
-                    <Link className="brand" to="/">Ora</Link>
-                </div>
 
-                <div>
-                    <Link to="/cart">Cart</Link>
-                    <Link to="/login">Sign In</Link>
-                </div>
-            </header>
+    // @ts-ignore
+    const cart = useSelector( state => state.cart);
+    const { cartItems } = cart;
 
-            <main>
-                <Route path="/" component={HomeScreen} exact></Route>
-                <Route path="/product/:id" component={ProductScreen}></Route>
-                <Route path="/cart/:id?" component={CartScreen}></Route>
-            </main>
+    return (
+        <BrowserRouter>
+            <div className="grid-container">
+                <header className="row">
+                    <div>
+                        <Link className="brand" to="/">Ora</Link>
+                    </div>
 
-            <footer className="row center">
-                All rights reserved
-            </footer>
-        </div>
-    </BrowserRouter>
-  );
+                    <div>
+                        <Link to="/cart">
+                            Cart
+                            {cartItems.length > 0 && (
+                                <span className="badge">{cartItems.length}</span>
+                            )}
+                        </Link>
+                        <Link to="/login">Sign In</Link>
+                    </div>
+                </header>
+
+                <main>
+                    <Route path="/" component={HomeScreen} exact></Route>
+                    <Route path="/product/:id" component={ProductScreen}></Route>
+                    <Route path="/cart/:id?" component={CartScreen}></Route>
+                </main>
+
+                <footer className="row center">
+                    All rights reserved
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 export default App;
