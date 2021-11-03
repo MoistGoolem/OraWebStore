7c3463f1804ac66a706f9d8e84fa3fbea2b50340
Oliver Ramstedt <ora@capasystems.com>
2021-11-03 14:46:12 +0100

    Setup and implemented register screen and api

--- backend/src/routers/userRouter.js
+++ backend/src/routers/userRouter.js
@@ -23,17 +23,42 @@ userRouter.post(
         const user = await User.findOne({ email: req.body.email });
         if(user) {
             if(bcrypt.compareSync(req.body.password, user.password)) {
-                res.send({
+                res.send(
+                    {
                     _id: user.id,
                     name: user.name,
                     email: user.email,
                     isAdmin: user.isAdmin,
                     token: generateToken(user)
-                });
+                    }
+                );
                 return;
             }
         } 
         res.status(401).send({ message: "Invalid email of password" });
+    })
+);
+
+userRouter.post(
+    '/register',
+    expressAsyncHandler(async (req, res) => {
+        const user = new User(
+            { 
+            name: req.body.name, 
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 12),
+            }
+        );
+        const createdUser = await user.save();
+        res.send(
+            {
+                _id: createdUser.id,
+                name: createdUser.name,
+                email: createdUser.email,
+                isAdmin: createdUser.isAdmin,
+                token: generateToken(createdUser)
+            }
+        );
     })
 );
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
-import { userLoginReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const initialState = {
     userLogin: {
@@ -22,6 +22,7 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 });
 
 // @ts-ignore
